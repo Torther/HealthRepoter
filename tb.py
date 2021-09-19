3e8e001891c583c60ca295c8f4940ad8d9f2aa7e
罗益宽 <36468339+luoyikuan@users.noreply.github.com>
2021-09-19 18:51:27 +0800

    Update tb.py

--- tb.py
+++ tb.py
@@ -1,1 +1,19 @@
+import os
+import requests
 
+# 学号
+loginId = os.environ["LOGIN_ID"]
+# 密码
+loginPwd = os.environ["LOGIN_PWD"]
+# 户籍
+householdRegister = os.environ["HOUSEHOLD_REGISTER"]
+# 手机号码
+phoneNumber = os.environ["PHONE_NUMBER"]
+# 当前位置
+currentLocation = os.environ["CURRENT_LOCATION"]
+
+print(loginId[0])
+print(loginPwd[0])
+print(householdRegister[0])
+print(phoneNumber[0])
+print(currentLocation[0])
