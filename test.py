020c5162fb091a4f4f536f5d48de598d2b969a4a
罗益宽 <36468339+luoyikuan@users.noreply.github.com>
2021-09-19 18:36:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ loginId = os.environ["LOGIN_ID"]
 loginPwd = os.environ["LOGIN_PWD"]
 # 户籍
 householdRegister = os.environ["HOUSEHOLD_REGISTER"]
-# 手机号码https://github.com/luoyikuan/wzpt_tb/blob/main/test.py
+# 手机号码
 phoneNumber = os.environ["PHONE_NUMBER"]
 # 当前位置
 currentLocation = os.environ["CURRENT_LOCATION"]
 
-print(loginId)
-print(loginPwd)
-print(householdRegister)
-print(phoneNumber)
-print(currentLocation)
+print(loginId[0])
+print(loginPwd[0])
+print(householdRegister[0])
+print(phoneNumber[0])
+print(currentLocation[0])
