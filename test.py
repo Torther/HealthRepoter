2d607f6caa90ec9eca63ba6f108b5d6e04127817
罗益宽 <36468339+luoyikuan@users.noreply.github.com>
2021-09-19 18:17:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("你好")
+import sys
+
+# 学号
+loginId = sys.argv[0]
+# 密码
+loginPwd = sys.argv[1]
+# 户籍
+householdRegister = sys.argv[2]
+# 手机号码
+phoneNumber = sys.argv[3]
+# 当前位置
+currentLocation = sys.argv[4]
+
+print(loginId)
+print(loginPwd)
+print(householdRegister)
+print(phoneNumber)
+print(currentLocation)
