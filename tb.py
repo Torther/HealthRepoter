537eda8215c38630435b0e4a19c1188625023aeb
罗益宽 <36468339+luoyikuan@users.noreply.github.com>
2021-09-20 10:03:37 +0800

    Update tb.py

--- tb.py
+++ tb.py
@@ -11,14 +11,80 @@ householdRegister = os.environ["HOUSEHOLD_REGISTER"]
 phoneNumber = os.environ["PHONE_NUMBER"]
 # 当前位置
 currentLocation = os.environ["CURRENT_LOCATION"]
+# 是否在校:0不在,1在
+isSchool = 1
 
-print(loginId[0])
-print(loginPwd[0])
-print(householdRegister[0])
-print(phoneNumber[0])
-print(currentLocation[0])
 
-r = requests.get("http://www.wzpt.edu.cn/")
-print(r.text)
+class TB:
+    def __init__(self, id, pwd, home, phone, city='浙江省温州市瓯海区', school=1):
+        self.__httpClient = requests.Session()
+        self.loginId = id
+        self.loginPwd = pwd
+        self.home = home
+        self.phone = phone
+        self.city = city
+        self.school = school
 
-print("结束")
+    def login(self):
+        url = 'https://info2.webvpn.wzvtc.cn/supply/check_wx.jsp'
+        data = {'name': self.loginId, 'abc': self.loginPwd}
+        loginResp = self.__httpClient.post(url=url, data=data)
+        return loginResp.status_code == 200 and loginResp.text.find(self.loginId)
+
+    def getMyBan(self):
+        url = 'https://info2.webvpn.wzvtc.cn/supply/disp_myban.jsp'
+        mybanResp = self.__httpClient.get(url)
+
+    def put(self):
+        # url = 'https://info2.webvpn.wzvtc.cn/supply/put.jsp'
+        url = 'https://info2.webvpn.wzvtc.cn/supply/wx/put.jsp'
+        charset = 'GBK'
+        req = {'b1': bytes(self.home, charset),
+               'b3': bytes(self.city, charset),
+               'b2': self.phone,
+               'a1': 0,
+               'a11': bytes('请点击并填写实测体温', charset),
+               'a2': 0,
+               'a21': bytes('请点击并填写实测体温', charset),
+               'a3': 0,
+               'a4': 0,
+               'a41': bytes('如有，请点击并填写说明', charset),
+               'a5': 0,
+               'a51': '',
+               'a52': '',
+               'a6': 0,
+               'a61': '',
+               'a62': '',
+               'a63': 0,
+               'a631': '',
+               'a7': 0,
+               'a71': '',
+               'a72': '',
+               'a73': '',
+               'a8': 1,
+               'a81': '',
+               'a9': 0,
+               'a91': '',
+               'a92': '',
+               'a93': '',
+               'a94': 0,
+               'aa': 0,
+               'ab': self.school,
+               'ac': 0,
+               'ac1': '',
+               'ac2': '',
+               'a91': '',
+               'a92': '',
+               'a93': ''}
+        resp = self.__httpClient.post(url=url, data=req)
+        return resp.status_code == 200 and (resp.text.find('填报成功！') or resp.text.find('已经申报成功，请勿重复提交！'))
+
+
+tb = TB(loginId, loginPwd, householdRegister,
+        phoneNumber, currentLocation, isSchool)
+if (tb.login() and tb.put()):
+    print("填报成功")
+else:
+    print("填报失败")
+
+
